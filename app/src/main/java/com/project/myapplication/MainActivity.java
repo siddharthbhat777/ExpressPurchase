c81fc867b0fded0e68c81d84710c9ece7819d879
siddharthbhat777 <dbgt4531@gmail.com>
2022-01-22 19:01:15 +0530

    night mode feature added
    Made a settings activity and had some changes in main Activity.

--- app/src/main/java/com/project/myapplication/MainActivity.java
+++ app/src/main/java/com/project/myapplication/MainActivity.java
@@ -1,16 +1,37 @@
 package com.project.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "new toast added", Toast.LENGTH_SHORT).show();
+        sharedPreferences = getSharedPreferences("night", 0);
+        boolean booleanValue = sharedPreferences.getBoolean("night_mode", true);
+        if (booleanValue) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Settings.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/project/myapplication/Settings.java
@@ -0,0 +1,49 @@
+package com.project.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+
+public class Settings extends AppCompatActivity {
+
+    SwitchCompat switchTheme;
+    SharedPreferences sharedPreferences = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        switchTheme = findViewById(R.id.night_switch_button);
+
+        sharedPreferences = getSharedPreferences("night", 0);
+        boolean booleanValue = sharedPreferences.getBoolean("night_mode", true);
+
+        if (booleanValue) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            switchTheme.setChecked(true);
+        }
+
+        switchTheme.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    switchTheme.setChecked(true);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("night_mode", true);
+                    editor.apply();
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    switchTheme.setChecked(false);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("night_mode", false);
+                    editor.apply();
+                }
+            }
+        });
+    }
+}
