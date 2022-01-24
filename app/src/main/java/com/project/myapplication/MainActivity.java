0ede565184f3b29a29e631f44402d29d31838ecf
siddharthbhat777 <dbgt4531@gmail.com>
2022-01-24 21:34:29 +0530

    added menu drawer and made changes in action bar

--- app/src/main/java/com/project/myapplication/MainActivity.java
+++ app/src/main/java/com/project/myapplication/MainActivity.java
@@ -5,24 +5,83 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView settingsTv;
+
     Button button;
     SharedPreferences sharedPreferences;
+
+    public static DrawerLayout mDrawerLayout;
+    private Toolbar mToolbar;
+
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Status Bar Color
+        getWindow().setStatusBarColor(ContextCompat.getColor(MainActivity.this, R.color.purple));
+
+        //Night Mode
         sharedPreferences = getSharedPreferences("night", 0);
         boolean booleanValue = sharedPreferences.getBoolean("night_mode", true);
         if (booleanValue) {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         }
+
+        // Setup Actionbar / Toolbar
+        mToolbar = (Toolbar) findViewById(R.id.action_bar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        mToolbar.setNavigationIcon(R.drawable.ic_menu_drawer);
+        //getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_drawer);
+        //getSupportActionBar().setLogo(R.drawable.action_bar_logo);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        // Setup Navigation Drawer Layout
+        mDrawerLayout=(DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.drawer_open, R.string.drawer_close) {
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        mDrawerLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mDrawerToggle.syncState();
+            }
+        });
+
+        settingsTv = findViewById(R.id.settingsTextView);
+
+        settingsTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Settings.class);
+                startActivity(intent);
+            }
+        });
 
         button = findViewById(R.id.button);
 
