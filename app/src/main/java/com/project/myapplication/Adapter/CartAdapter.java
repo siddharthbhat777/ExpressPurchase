3294761322942b03b1346e358ba21e41a21558d0
siddharthbhat777 <dbgt4531@gmail.com>
2022-02-05 16:27:32 +0530

    changes made

--- app/src/main/java/com/project/myapplication/Adapter/CartAdapter.java
+++ app/src/main/java/com/project/myapplication/Adapter/CartAdapter.java
@@ -8,6 +8,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.project.myapplication.Model.CartModel;
@@ -49,8 +50,8 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.ViewHolder> {
         price = model.getItemPrice();
 
         Picasso.get().load(model.getItemImage()).into(holder.thumbnail);
-        holder.tittle.setText(model.getItemName());
-        holder.price.setText("Rs " + String.valueOf(model.getItemPrice()));
+        holder.title.setText(model.getItemName());
+        holder.price.setText("₹ " + String.valueOf(model.getItemPrice()));
         holder.quantity.setText(String.valueOf(quantity));
 
         holder.plus.setOnClickListener(new View.OnClickListener() {
@@ -63,7 +64,7 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.ViewHolder> {
                 else {
                     price = price + model.getItemPrice();
 
-                    holder.price.setText("Rs " + String.valueOf(price));
+                    holder.price.setText("₹ " + String.valueOf(price));
                     quantity++;
                     holder.quantity.setText(String.valueOf(quantity));
                 }
@@ -79,7 +80,7 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.ViewHolder> {
 
                 } else {
                     price = price - model.getItemPrice();
-                    holder.price.setText("Rs " + String.valueOf(price));
+                    holder.price.setText("₹ " + String.valueOf(price));
                     quantity--;
 
                     holder.quantity.setText(String.valueOf(quantity));
@@ -110,19 +111,20 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.ViewHolder> {
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        ImageView thumbnail, plus, minus;
-        TextView tittle, price, quantity, remove;
+        ImageView thumbnail;
+        TextView title, price, quantity;
+        CardView plus, minus, remove;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             thumbnail = itemView.findViewById(R.id.img);
-            plus = itemView.findViewById(R.id.cart_plus_img);
-            minus = itemView.findViewById(R.id.cart_minus_img);
-            tittle = itemView.findViewById(R.id.name);
+            plus = itemView.findViewById(R.id.plus_cart_card);
+            minus = itemView.findViewById(R.id.minus_cart_card);
+            title = itemView.findViewById(R.id.name);
             price = itemView.findViewById(R.id.price_text);
             quantity = itemView.findViewById(R.id.cart_product_quantity_tv);
-            remove = itemView.findViewById(R.id.remove_cart);
+            remove = itemView.findViewById(R.id.remove_cart_card);
 
         }
     }
