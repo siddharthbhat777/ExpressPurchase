6a8d8bd77cc8e010735fcd6e05cc3da18ab34083
siddharthbhat777 <dbgt4531@gmail.com>
2022-02-06 16:01:32 +0530

    splash screen error

--- app/src/main/java/com/project/myapplication/Activities/SplashActivity.java
+++ app/src/main/java/com/project/myapplication/Activities/SplashActivity.java
@@ -26,37 +26,81 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.project.myapplication.R;
+import com.project.myapplication.databinding.ActivityMainBinding;
 
 import java.util.HashMap;
 
 public class SplashActivity extends AppCompatActivity {
-    private static final int RC_SIGN_IN = 0;
-    private GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 1;
+    private static final String TAG = "SplashActivity";
+    ActivityMainBinding binding;
+    GoogleSignInOptions gso;
+    GoogleSignInClient mGoogleSignInClient;
     private FirebaseAuth mAuth;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        GoogleSignInOptions gso = new GoogleSignInOptions
-                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setupforsignin();
+        Signinwithgooge();
+
+
+    }
+    private void setupforsignin() {
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // Configure Google Sign In
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
-
-
-                // web secreat id :- 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
-                // web client id :- 1060018672946-40ipgpeomre764gse0dpeo9phc95gg8a.apps.googleusercontent.com
-
                 .build();
-        mAuth = FirebaseAuth.getInstance();
+
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        signIn();
+    }
+
+
+    private void Signinwithgooge() {
+
+                signin();
 
     }
-    private void signIn() {
+
+    private void signin() {
+
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
+
+
     }
+
+    @Override
+    protected void onStart() {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        super.onStart();
+
+        if (currentUser != null) {
+            Thread thread = new Thread(){
+                @Override
+                public void run() {
+                    try {
+                        sleep(2000);
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        finish();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    super.run();
+                }
+
+            };thread.start();
+
+        } else {
+            Signinwithgooge();
+        }
+    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -67,29 +111,31 @@ public class SplashActivity extends AppCompatActivity {
             try {
                 // Google Sign In was successful, authenticate with Firebase
                 GoogleSignInAccount account = task.getResult(ApiException.class);
-                firebaseAuthWithGoogle(account.getIdToken());
+                firebaseAuthWithGoogle(account.getIdToken(), account.getId(), account.getEmail(), account.getDisplayName(), account.getPhotoUrl().toString());
             } catch (ApiException e) {
-                // Google Sign In failed, update UI appropriately
-                Log.println(Log.ASSERT,"sign-in", "failed"+ e.getStatusCode());
+                Toast.makeText(SplashActivity.this, "Failed ! Something wrong happens", Toast.LENGTH_SHORT).show();
+                Log.e(TAG, "onActivityResult: " + e.getLocalizedMessage());
+
             }
         }
     }
-    private void firebaseAuthWithGoogle(String idToken) {
-        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+
+    private void firebaseAuthWithGoogle(String token, String idToken, String gmail, String name, String photourl) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(token, null);
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            Intent intent = new Intent(getApplicationContext(),MainActivity.class); // intent to go to mainactivity if the user is sucesfully sign n
-                            if(getIntent().getExtras()!=null) intent.putExtras(getIntent().getExtras());
-                            startActivity(intent);
-                            finish();
+
+
+
                         } else {
                             // If sign in fails, display a message to the user.
-                            Log.println(Log.ASSERT, "failure", task.getException().toString());
+                            Toast.makeText(SplashActivity.this, "Something went wrong !", Toast.LENGTH_SHORT).show();
+                            Log.e(TAG, "onActivityResult: " + task.getException().getLocalizedMessage());
+
                         }
                     }
                 });
