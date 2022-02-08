a6b3680deadf48330d14c7908494eff8ba28ca6a
siddharthbhat777 <dbgt4531@gmail.com>
2022-02-08 23:40:25 +0530

    added payment options layout

---
+++ app/src/main/java/com/project/myapplication/Activities/PaymentOptions.java
@@ -0,0 +1,14 @@
+package com.project.myapplication.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaymentOptions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_options);
+    }
+}
