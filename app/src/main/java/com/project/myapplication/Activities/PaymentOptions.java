dc5b73ab72938b7f4ef8a3616c8b0cacd3f52f77
siddharthbhat777 <dbgt4531@gmail.com>
2022-02-09 19:41:21 +0530

    created UI of View Orders

--- app/src/main/java/com/project/myapplication/Activities/PaymentOptions.java
+++ app/src/main/java/com/project/myapplication/Activities/PaymentOptions.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.project.myapplication.R;
+
 public class PaymentOptions extends AppCompatActivity {
 
     @Override
