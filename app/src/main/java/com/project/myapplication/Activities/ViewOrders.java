e0a1a104bd41418b7c496e4bbddd2836a68308ef
Ajaz <help.discoverhitech@gmail.com>
2022-03-19 13:15:37 +0530

    VIEW ORDER DIALOG DONE

--- app/src/main/java/com/project/myapplication/Activities/ViewOrders.java
+++ app/src/main/java/com/project/myapplication/Activities/ViewOrders.java
@@ -1,13 +1,18 @@
 package com.project.myapplication.Activities;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -20,8 +25,8 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.project.myapplication.Adapter.ViewOrderAdapter;
-import com.project.myapplication.R;
 import com.project.myapplication.Model.ViewOrderModel;
+import com.project.myapplication.R;
 import com.project.myapplication.databinding.ActivityViewOrdersBinding;
 
 import java.text.SimpleDateFormat;
@@ -41,26 +46,19 @@ public class ViewOrders extends AppCompatActivity {
         setContentView(binding.getRoot());
         setdata();
         initrv();
+        intlistner();
 
     }
 
-    private void onclick(String invoice_number) {
-        binding.viewOrdersButton.setOnClickListener(new View.OnClickListener() {
+    private void intlistner() {
+        binding.materialCardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_SEND);
-                i.setType("message/rfc822");
-                i.putExtra(Intent.EXTRA_EMAIL, new String[]{"expresspurchase4531@gmail.com"});
-                i.putExtra(Intent.EXTRA_SUBJECT, "My Order Problem");
-                i.putExtra(Intent.EXTRA_TEXT, "Your Query Here...... \n\n\n\n\n\n\n" + "Product Invoice Number : " + invoice_number);
-                try {
-                    startActivity(Intent.createChooser(i, "Send mail..."));
-                } catch (android.content.ActivityNotFoundException ex) {
-                    Toast.makeText(ViewOrders.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
-                }
+
             }
         });
     }
+
 
     private void setdata() {
 
@@ -93,8 +91,23 @@ public class ViewOrders extends AppCompatActivity {
                     binding.textView26.setText(time);
 
 
-                    onclick(model.getInvoice_number());
+                    FirebaseFirestore.getInstance().collection("User").document(account.getEmail()).addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                        @Override
+                        public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                            if (value != null) {
+                                String address = value.getString("address");
 
+                                binding.viewOrdersButton.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        ViewDialog alert = new ViewDialog();
+                                        alert.showDialog(ViewOrders.this, model.getItem_name(), model.getInvoice_number(), model.getNumber_of_products(), model.getTotal_price(),
+                                                model.getDate_of_delivery(), address);
+                                    }
+                                });
+                            }
+                        }
+                    });
                 } else {
                     Toast.makeText(getApplicationContext(), "Nothing Here", Toast.LENGTH_SHORT).show();
                 }
@@ -128,5 +141,75 @@ public class ViewOrders extends AppCompatActivity {
         rv_orders.setLayoutManager(new LinearLayoutManager(this));
         rv_orders.setAdapter(adapter);
 
+    }
+
+    public class ViewDialog {
+
+        public void showDialog(Activity activity, String item_name, String invoice, String quantitys, String prices, long dateofdelivery, String addres) {
+            final Dialog dialog = new Dialog(activity);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+            dialog.setCancelable(false);
+            dialog.setContentView(R.layout.order_details);
+
+            CardView contact = (CardView) dialog.findViewById(R.id.materialCardView6);
+            CardView cancell = (CardView) dialog.findViewById(R.id.cancel);
+
+
+            TextView name = dialog.findViewById(R.id.tvNa);
+            name.setText(item_name);
+
+
+            TextView invoiceno = dialog.findViewById(R.id.tvInv);
+            invoiceno.setText(invoice);
+
+
+            TextView quantity = dialog.findViewById(R.id.tvNoOfProducts);
+            quantity.setText(quantitys);
+
+
+            TextView price = dialog.findViewById(R.id.tvTotalPrice);
+            price.setText(prices);
+
+            TextView address = dialog.findViewById(R.id.tvAddress);
+            address.setText(addres);
+
+
+            TextView date = dialog.findViewById(R.id.tvDate);
+
+
+            SimpleDateFormat sdf = new SimpleDateFormat("MMM dd,yyyy HH:mm");
+            Date resultdate = new Date(dateofdelivery);
+
+            date.setText(sdf.format(resultdate));
+
+
+            cancell.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+
+            contact.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(Intent.ACTION_SEND);
+                    i.setType("message/rfc822");
+                    i.putExtra(Intent.EXTRA_EMAIL, new String[]{"expresspurchase4531@gmail.com"});
+                    i.putExtra(Intent.EXTRA_SUBJECT, "My Order Problem");
+                    i.putExtra(Intent.EXTRA_TEXT, "Your Query Here...... \n\n\n\n\n\n\n" + "Product Invoice Number : " + invoice);
+                    try {
+                        activity.startActivity(Intent.createChooser(i, "Send mail..."));
+                    } catch (android.content.ActivityNotFoundException ex) {
+                        Toast.makeText(activity, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+            dialog.show();
+
+        }
     }
 }
